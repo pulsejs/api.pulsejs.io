e7f733cdbf58637ffbb17dcde747576df6820e26
Wilmer H. Muñoz <wilmerhmg@gmail.com>
2018-07-19 20:48:33 -0500

    No public

--- app.js
+++ app.js
@@ -20,8 +20,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+//app.use(express.static(path.join(__dirname, 'public')));
 app.use('/v1/:v1*?', function (req, res, next) {
 	let credentials = auth(req);
 
